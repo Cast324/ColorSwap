1ebe386a228ce7c59ba3758813e8013d54bbef21
Michael Blades <michaelblades12@gmail.com>
2024-04-02 20:48:07 -0500

    Initial Commit

---
+++ ColorSwap/ColorSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSwapApp.swift
+//  ColorSwap
+//
+//  Created by Michael Blades on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
